d9d07282697c297d9471a9e17a3d4f9a3bbecf7d
Kohei Noda <ponserviceregist@gmail.com>
2023-10-14 13:36:54 +0900

    Add the types directory to provide API types to the Next.js

---
+++ src/types/note.ts
@@ -0,0 +1,3 @@
+import { type RouterOutputs } from "~/utils/api";
+
+export type Note = RouterOutputs["note"]["getAll"][0];
---
+++ src/types/topic.ts
@@ -0,0 +1,3 @@
+import { type RouterOutputs } from "~/utils/api";
+
+export type Topic = RouterOutputs["topic"]["getAll"][0];
